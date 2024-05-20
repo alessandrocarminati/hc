45c2d4fb4fdf6b2dad332762164ed81274121688
Alessandro Carminati <alessandro.carminati@gmail.com>
2024-05-20 09:58:44 +0200

    tree: refactored
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- tree.go
+++ tree.go
@@ -24,27 +24,36 @@ func buildLogTree(logs []string) (*LogTree, error) {
 	tree := &LogTree{Year: make(map[int]*YearNode)}
 
 	for _, logStr := range logs {
-		entry, err := parseLogEntry(logStr)
-		if err != nil {
+		err := ProcessEntryTree(logStr, tree)
+		if err!=nil {
 			return nil, err
 		}
-
-		year := entry.Timestamp.Year()
-		month := entry.Timestamp.Month()
-		day := entry.Timestamp.Day()
-
-		if tree.Year[year] == nil {
-			tree.Year[year] = &YearNode{Month: make(map[time.Month]*MonthNode)}
-		}
-		if tree.Year[year].Month[month] == nil {
-			tree.Year[year].Month[month] = &MonthNode{Day: make(map[int]*DayNode)}
-		}
-		if tree.Year[year].Month[month].Day[day] == nil {
-			tree.Year[year].Month[month].Day[day] = &DayNode{Session: make(map[string][]LogEntry)}
-		}
-
-		tree.Year[year].Month[month].Day[day].Session[entry.SessionID] = append(tree.Year[year].Month[month].Day[day].Session[entry.SessionID], entry)
 	}
 
 	return tree, nil
 }
+
+func ProcessEntryTree(logStr string, tree *LogTree) (error) {
+	entry, err := parseLogEntry(logStr)
+	if err != nil {
+		return err
+	}
+
+	year := entry.Timestamp.Year()
+	month := entry.Timestamp.Month()
+	day := entry.Timestamp.Day()
+
+	if tree.Year[year] == nil {
+		tree.Year[year] = &YearNode{Month: make(map[time.Month]*MonthNode)}
+	}
+	if tree.Year[year].Month[month] == nil {
+		tree.Year[year].Month[month] = &MonthNode{Day: make(map[int]*DayNode)}
+	}
+	if tree.Year[year].Month[month].Day[day] == nil {
+		tree.Year[year].Month[month].Day[day] = &DayNode{Session: make(map[string][]LogEntry)}
+	}
+
+	tree.Year[year].Month[month].Day[day].Session[entry.SessionID] = append(tree.Year[year].Month[month].Day[day].Session[entry.SessionID], entry)
+	return nil
+}
+

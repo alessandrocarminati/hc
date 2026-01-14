6fd23d0a6ce93921d09b2140867c3644f686dd04
Alessandro Carminati <alessandro.carminati@gmail.com>
2026-01-14 22:47:22 +0100

    Example function updated
    The payload injected into the remote host has been
    added to inject also a function to allow remote
    machine to query the collected history.
    This version also fix to old corner cases:
    1. no more sent the old history on connection
    2. no more send a copy of the previous on empty
       line
    
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- example.bash
+++ example.bash
@@ -1,37 +1,41 @@
 sshl() {
-    local port="12345"
-    local mode="1"
-    local verify="0"
-    local use_gzip=false
+    local iport="1234"
+    local sport="8080"
+    local imode="1"
+    local iverify="0"
+    local nuse_gzip=false
     local OPTIND
-    local hc_host=wiki-ng.hqhome163.com
+    local hc_host=auto.hqhome163.com
 
-    while getopts "p:m:v:z" opt; do
+    while getopts "p:m:v:z:s" opt; do
         case "$opt" in
-            p) port=$OPTARG ;;
-            m) mode=$OPTARG ;;
-            v) verify=$OPTARG ;;
-            z) use_gzip=true ;;
+            p) iport=$OPTARG ;;
+            m) imode=$OPTARG ;;
+            v) iverify=$OPTARG ;;
+            z) nuse_gzip=true ;;
+            s) sport=$OPTARG ;;
+
         esac
     done
     shift $((OPTIND-1))
 
     local cmd_base='export SESSIONID_=$(date +%Y%m%d.%H%M%S |sha1sum | cut -c1-8); '
     local log_content='$(date +%Y%m%d.%H%M%S) - ${SESSIONID_} - $(hostname --fqdn) [cwd=$(pwd)] > $(history -w /dev/stdout | tail -n1)'
+    local func_defs="l4h(){ wget -qO- -- \"http://127.0.0.1:${sport}/export?grep1=\$1&color=always\"; }; export -f l4h;"
 
     local transport=""
-    if [ "$mode" = "2" ]; then
-        transport="socat - OPENSSL:127.0.0.1:${port},verify=${verify}"
+    if [ "$imode" = "2" ]; then
+        transport="socat - OPENSSL:127.0.0.1:${iport},verify=${iverify}"
     else
-        transport="nc 127.0.0.1 ${port}"
+        transport="nc 127.0.0.1 ${iport}"
     fi
 
-    local full_payload="${cmd_base} export PROMPT_COMMAND='echo \"${log_content}\" | ${transport}'; exec bash -i"
+    local full_payload="${cmd_base}${func_defs} export PROMPT_COMMAND='if [ -z \"\$LAST_H_\" ]; then LAST_H_=\$(history 1| sed -r \"s/ *([0-9]+).*/\1/\"); elif [ \"\$(history 1| sed -r \"s/ *([0-9]+).*/\1/\")\" -ne \"\$LAST_H_\" ]; then LAST_H_=\"\$(history 1| sed -r \"s/ *([0-9]+).*/\1/\")\"; echo \"${log_content}\" | ${transport}; fi'; exec bash -i"
 
     local encoded_payload
     local remote_decode_cmd
 
-    if [ "$use_gzip" = true ]; then
+    if [ "$nuse_gzip" = true ]; then
         encoded_payload=$(echo "$full_payload" | gzip -c | base64 | tr -d '\n')
         remote_decode_cmd="eval \$(echo ${encoded_payload} | base64 -d | gunzip -c)"
     else
@@ -39,5 +43,5 @@ sshl() {
         remote_decode_cmd="eval \$(echo ${encoded_payload} | base64 -d)"
     fi
 
-    ssh -t -R "${port}:${hc_host}:${port}" "$@" "$remote_decode_cmd"
+    ssh -t -R "${iport}:${hc_host}:${iport}" -R "${sport}:${hc_host}:${sport}" "$@" "$remote_decode_cmd"
 }

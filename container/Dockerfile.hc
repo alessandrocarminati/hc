6176a6282cbe6b7658b8b70fe14f05ab4a33a8b5
Alessandro Carminati <acarmina@redhat.com>
2026-01-16 10:39:38 +0100

    hc container - first draft
    Signed-off-by: Alessandro Carminati <acarmina@redhat.com>

---
+++ container/Dockerfile.hc
@@ -0,0 +1,30 @@
+# syntax=docker/dockerfile:1.6
+
+FROM golang:1.22-alpine AS build
+WORKDIR /src
+
+RUN apk add --no-cache git make bash
+
+RUN git clone https://github.com/alessandrocarminati/hc .
+
+RUN set -eux; \
+    make
+
+RUN set -eux; \
+    mkdir -p /out; \
+    cp -L hc.app /out/hc
+
+FROM alpine:3.20
+WORKDIR /app
+
+RUN apk add --no-cache gettext
+
+COPY --from=build /out/hc /usr/local/bin/hc
+
+COPY --from=build /src/container/hc-config.template.json /config/hc-config.template.json
+
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+EXPOSE 1234 1235 8080 8443
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ container/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -eu
+
+# ---- TCP ports ----
+: "${iport_clear:=1234}"
+: "${iport_ssl:=1235}"
+: "${sport_http:=8080}"
+: "${sport_https:=8443}"
+
+# ---- DB defaults  ----
+: "${db_host:=db}"
+: "${db_port:=5432}"
+: "${db_user:=hc}"
+: "${db_password:=hc}"
+: "${db_ssl_mode:=disable}"
+
+# ---- app defaults ----
+: "${default_tenant_uuid:=9b9b1a6e-3d3e-4b2a-8f2c-3a51b84e4a0a}"
+: "${random_string:=dev-pepper-change-me}"
+
+# ---- cert paths inside container ----
+: "${ca_certificate:=/certs/ca.crt}"
+: "${host_certificate:=/certs/host.crt}"
+: "${host_key:=/certs/host.key}"
+
+mkdir -p /config
+envsubst < /config/hc-config.template.json > /config/hc-config.json
+
+echo "HC using DB: ${db_user}@${db_host}:${db_port} sslmode=${db_ssl_mode}"
+exec /usr/local/bin/hc serve --config /config/hc-config.json
